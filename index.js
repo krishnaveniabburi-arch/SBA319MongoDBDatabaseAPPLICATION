a1ed0339a5c82a54d742e2975cad18c851d9d6ee
krishnaveniabburi-arch <krishnaveni.abburi@gmail.com>
2026-07-13 22:40:13 -0400

    add new logic

--- index.js
+++ index.js
@@ -6,12 +6,21 @@ import connectDB from "./db/db.js";
 
 import mongoose from "mongoose";
 
+import userRoutes from "./routes/user1.js";
+import postRoutes from "./routes/post1.js";
+import commentRoutes from "./routes/comment1.js";
+
+
 dotenv.config(); 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.use("/user1", userRoutes);
+app.use("/post1", postRoutes);
+app.use("/comment1", commentRoutes);
 
 app.get("/", (req,res) => {
     res.send("result")
